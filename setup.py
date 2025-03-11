dd123b67c396b16bd97c2956cb133960ea6a3dbb
Pieter Provoost <pieterprovoost@gmail.com>
2025-03-11 21:07:30 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gnparser",
-    version="0.1.1",
+    version="0.1.2",
     description="Python wrapper for the gnparser Go library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
